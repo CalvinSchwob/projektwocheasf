5d4336d2caec100d8278b6af18051a2a54d2b6cf
CalvinSchwob <90397712+CalvinSchwob@users.noreply.github.com>
2026-03-20 06:52:42 +0000

    Update db test

--- test_db.php
+++ test_db.php
@@ -1,3 +1,19 @@
-INSERT INTO mitarbeiter (email, passwort) VALUES 
-('admin@optiker.de', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'),
-('mitarbeiter@optiker.de', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once "config.php";
+
+$sql = "SELECT * FROM mitarbeiter";
+$result = mysqli_query($link, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "DB OK! Gefundene Mitarbeiter:<br>";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "ID: " . $row["id"] . " - " . $row["email"] . "<br>";
+    }
+} else {
+    echo "Keine Daten gefunden oder Tabelle leer.";
+}
+mysqli_close($link);
+?>
