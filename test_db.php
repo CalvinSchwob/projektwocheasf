a2a49e939d6d67b11ebb2b8777063995bd3d11cb
CalvinSchwob <90397712+CalvinSchwob@users.noreply.github.com>
2026-03-20 06:49:56 +0000

    Add db test

---
+++ test_db.php
@@ -0,0 +1,3 @@
+INSERT INTO mitarbeiter (email, passwort) VALUES 
+('admin@optiker.de', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'),
+('mitarbeiter@optiker.de', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
